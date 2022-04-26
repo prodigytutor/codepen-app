afdaa198d8033af0cce679e6353872bebb672eb6
RyanKendrick <rpkendrick@gmail.com>
2022-04-26 10:50:53 -0400

    progress

--- components/codepen/Editor.tsx
+++ components/codepen/Editor.tsx
@@ -1,54 +1,8 @@
-import React, { useState, FunctionComponent } from 'react'
-import 'codemirror/lib/codemirror.css'
-import 'codemirror/theme/material.css'
-import 'codemirror/mode/xml/xml'
-import 'codemirror/mode/javascript/javascript'
-import 'codemirror/mode/css/css'
-import { Controlled } from 'react-codemirror2'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCompressAlt, faExpandAlt } from '@fortawesome/free-solid-svg-icons'
+import React, { FunctionComponent } from 'react'
 
 const Editor: FunctionComponent = () => {
-
-    const {
-        language,
-        displayName,
-        value,
-        onChange
-    } = props
-
-    const [open, setOpen] = useState(true)
-
-    function handleChange(editor, data, value) {
-        onChange(value)
-    }
-
   return (
-    <>
-    <div className={`editor-container ${open ? '' : 'collapsed'}`}>
-        <div className='editor-title'>
-            {displayName}
-            <button
-                type='button'
-                className='expand-collapse-button'
-                onClick={() => setOpen(prevOpen => !prevOpen)}    
-            >
-                <FontAwesomeIcon icon={open ? faCompressAlt : faExpandAlt} />
-            </button>
-        </div>
-        <Controlled
-            onBeforeChange={handleChange}
-            value={value}
-            options={{
-                lineWrapping: true,
-                lint: true,
-                lineNumbers: true,
-                mode: language,
-                theme: 'material'
-            }} 
-        />
-    </div>
-    </>
+    <div>Editor</div>
   )
 }
 
--- pages/codepen.tsx
+++ pages/codepen.tsx
@@ -1,11 +1,66 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import type { NextPage } from 'next';
 import Editor from '../components/codepen/Editor';
+import { htmlGameboy, cssGameboy, jsGameboy } from '../components/codepen/gameboy-code';
 
 const Codepen: NextPage = () => {
+
+  const [html, setHtml] = useState(htmlGameboy)
+  const [css, setCss] = useState(cssGameboy)
+  const [js, setJs] = useState(jsGameboy)
+  const [srcDoc, setSrcDoc] = useState('')
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setSrcDoc(`
+      <html>
+          <body>${html}</body>
+          <style>${css}</style>
+          <script>${js}</script>
+      </html>
+      `)
+    }, 250)
+    return () => clearTimeout(timeout)
+    
+  }, [html, css, js])
+
   return (
     <>
-      <Editor />
+      <div>
+            {/* Codepen */}
+            <div className='pane top-pane'>
+                <Editor 
+                    // language='xml'
+                    // displayName='HTML'
+                    // value={html}
+                    // onChange={setHtml}
+                />
+                <Editor 
+                    // language='javascript'
+                    // displayName='JS'
+                    // value={js}
+                    // onChange={setJs}
+                />
+                <Editor 
+                    // language='css'
+                    // displayName='CSS'
+                    // value={css}
+                    // onChange={setCss}
+                />
+            </div>
+            <div className='pane'>
+                <iframe
+                    srcDoc={srcDoc}
+                    title='output'
+                    sandbox='allow-scripts'
+                    frameBorder='0'
+                    width='100%'
+                    height='100%'
+                    className='iframe-codepen-page'
+
+                />
+            </div>
+        </div>
     </>
   )
 }
