4a540d27cc5894ea82b33a34db9ffecf0b167b13
RyanKendrick <rpkendrick@gmail.com>
2022-05-03 09:43:48 -0400

    progress

--- components/index/Section1.tsx
+++ components/index/Section1.tsx
@@ -1,8 +1,6 @@
 import React, { FunctionComponent } from 'react'
 import Image from 'next/image'
 
-
-
 const Section1: FunctionComponent = () => {
   return (
     <>
@@ -18,7 +16,11 @@ const Section1: FunctionComponent = () => {
             <h1 className='section1-title'>The best place to build, test, and discover front-end code.</h1><br/>
             <p className='section1-subtitle'> CodePen is a <b>social development environment</b> for front-end designers and developers. Build and deploy a website, show off your work, build test cases to learn and debug, and find inspiration.</p>
         </div>
-        <div className='container2'>section 2</div>
+        <div className='container2'>
+            <div className="codepen-example1">codepen example</div>
+            <div className="codepen-example2">codepen example</div>
+            <div className="codepen-example3">codepen example</div>
+        </div>
     </div>
     </>
   )
