34bcdf657c5731c72b87c894139c9480c1cec9ac
RyanKendrick <rpkendrick@gmail.com>
2022-05-04 09:57:20 -0400

    progress

--- components/index/Header.tsx
+++ components/index/Header.tsx
@@ -6,24 +6,26 @@ const Header: FunctionComponent = () => {
     <>
     <div className='header'>
         {/* icons */}
-        <a href="">
-        <Image 
-            height={25}
-            width={25}
-            alt={`search`}
-            src={`/codepen-icon.svg`}
-            className='header-icons'
-            />
-        </a>
-        <button>
-          <Image
-              height={25}
-              width={25}
-              alt={`search`}
-              src={`/menu.svg`}
-              className='header-icons'
-              />
-        </button>
+        <div className='mobile-icons'>
+          <a className='mobile-href' href="">
+            <Image
+                height={45}
+                width={45}
+                alt={`search`}
+                src={`/codepen-icon-small.svg`}
+                className='header-icons'
+                />
+          </a>
+          <button className='mobile-btn'>
+            <Image
+                height={35}
+                width={42}
+                alt={`search`}
+                src={`/menu.svg`}
+                className='header-icons'
+                />
+          </button>
+        </div>
         <form className='search-form' action="">
           <Image 
             height={25}
